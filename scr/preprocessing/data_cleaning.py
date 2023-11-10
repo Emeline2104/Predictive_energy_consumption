49345b6a50fe9c9c1ff133c7e904cb97592381d4
Emeline2104 <emeline.tapin@gmail.com>
2023-11-10 13:00:38 +0100

    Mise en page

--- scr/preprocessing/data_cleaning.py
+++ scr/preprocessing/data_cleaning.py
@@ -22,7 +22,6 @@ Afficher les données nettoyées.
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
 class Cleaning:
     """
     Regroupe les méthodes de nettoyage spécifiques, comme l'extraction des types de bâtiments,
@@ -40,8 +39,8 @@ class Cleaning:
         """
         self.data = data
         # Initialisation des attributs vide de split data
-        self.X_train = None
-        self.X_test = None
+        self.x_train = None
+        self.x_test = None
         self.y_train = None
         self.y_test = None
 
@@ -172,11 +171,12 @@ class Cleaning:
 
     def split_data(self):
         """
-        Prépare les données en séparant les features et la target, effectue la stratification et renvoie les ensembles d'entraînement et de test.
+        Prépare les données en séparant les features et la target, effectue la 
+        stratification et renvoie les ensembles d'entraînement et de test.
 
         Returns:
-        X_train (pd.DataFrame): Ensemble d'entraînement des features.
-        X_test (pd.DataFrame): Ensemble de test des features.
+        x_train (pd.DataFrame): Ensemble d'entraînement des features.
+        x_test (pd.DataFrame): Ensemble de test des features.
         y_train (pd.DataFrame): Ensemble d'entraînement de la target.
         y_test (pd.DataFrame): Ensemble de test de la target.
         """
@@ -184,24 +184,25 @@ class Cleaning:
 
         # Crée une table de features en supprimant les colonnes d'indicateurs de consommation
         features = self.data.drop(columns=indic_conso)
-            
+
         # Crée une table de target en incluant uniquement les colonnes d'indicateurs de consommation
         target = self.data[indic_conso]
-            
+
         # Convertit la colonne 'TypeUtilisationPrincipale' en chaîne de caractères
         features['TypeUtilisationPrincipale'] = features['TypeUtilisationPrincipale'].astype(str)
-            
-        # Stratification sur la variable catégorielle pour obtenir X_train et X_test
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            features, target, test_size=0.2, random_state=None, stratify=features['TypeUtilisationPrincipale']
+
+        # Stratification sur la variable catégorielle pour obtenir x_train et x_test
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
+            features, target, test_size=0.2, random_state=None,
+            stratify=features['TypeUtilisationPrincipale'],
         )
-            
-        print("Dimensions de l'ensemble d'entraînement :", self.X_train.shape, self.y_train.shape)
-        print("Dimensions de l'ensemble de test :", self.X_test.shape, self.y_test.shape)
-            
+
+        print("Dimensions de l'ensemble d'entraînement :", self.x_train.shape, self.y_train.shape)
+        print("Dimensions de l'ensemble de test :", self.x_test.shape, self.y_test.shape)
+
         # Conversion des arrays NumPy en DataFrames
-        self.X_train = pd.DataFrame(self.X_train, columns=features.columns)
-        self.X_test = pd.DataFrame(self.X_test, columns=features.columns)
+        self.x_train = pd.DataFrame(self.x_train, columns=features.columns)
+        self.x_test = pd.DataFrame(self.x_test, columns=features.columns)
         self.y_train = pd.DataFrame(self.y_train, columns=target.columns)
         self.y_test = pd.DataFrame(self.y_test, columns=target.columns)
 
@@ -235,16 +236,11 @@ class DataCleaningPipeline:
         self.cleaning_instance.remove_missing_data()
         self.cleaning_instance.apply_usage_correspondence()
         self.cleaning_instance.split_data()
-        return self.cleaning_instance.X_train, self.cleaning_instance.X_test, self.cleaning_instance.y_train, self.cleaning_instance.y_test
+        
+        return (
+            self.cleaning_instance.x_train,
+            self.cleaning_instance.x_test,
+            self.cleaning_instance.y_train,
+            self.cleaning_instance.y_test
+        )
     
-# Utilisation
-data_exemple = pd.read_csv(
-    '/Users/beatricetapin/Documents/2023/Data Science/Projet 4/Projet_4_TAPIN/data/raw/2016_Building_Energy_Benchmarking.csv'
-    )
-
-# Création d'une instance de DataCleaningPipeline avec Cleaning comme étape
-pipeline = DataCleaningPipeline(Cleaning(data_exemple))
-
-# Nettoyage des données
-X_train, X_test, y_train, y_test  = pipeline.clean()
-print(X_train.head())
